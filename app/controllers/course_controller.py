180605b402476b05f97b07b153d5ccef84af59b9
Hari Krishnan <harivc64@gmail.com>
2025-09-02 11:39:22 +0530

    removing legacy code

--- app/controllers/course_controller.py
+++
@@ -1,9 +0,0 @@
-from app.db.connection import get_connection
-
-def get_all_courses():
-    conn = get_connection()
-    cursor = conn.cursor(dictionary=True)
-    cursor.execute("SELECT * FROM courses")
-    rows = cursor.fetchall()
-    conn.close()
-    return rows
--- app/routers/routes.py
+++
@@ -1,8 +0,0 @@
-from fastapi import APIRouter
-
-from app.routers.course_router import router as course_router
-
-router = APIRouter()
-
-# Register all routers here
-router.include_router(course_router, prefix="/courses")
