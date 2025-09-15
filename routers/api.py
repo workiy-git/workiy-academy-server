4d7f2644cb3e0f756284585e05047f7b38a4c3a0
Hari Krishnan <harivc64@gmail.com>
2025-09-15 17:27:12 +0530

    common routes

--- routers/api.py
+++ routers/api.py
@@ -7,8 +7,8 @@ from .newsletter import router as newsletter_router
 
 api_router = APIRouter()
 
-api_router.include_router(course_router, prefix="/courses")
-api_router.include_router(course_details_router, prefix="/course-details")
-api_router.include_router(enquiry_router, prefix="/enquiries")
-api_router.include_router(internship_router, prefix="/internships")
-api_router.include_router(newsletter_router, prefix="/newsletter")
+api_router.include_router(course_router, prefix="/courses", tags=["Courses"])
+api_router.include_router(course_details_router, prefix="/course-details", tags=["Course Details"])
+api_router.include_router(enquiry_router, prefix="/enquiries", tags=["Enquiries"])
+api_router.include_router(internship_router, prefix="/internships", tags=["Internships"])
+api_router.include_router(newsletter_router, prefix="/newsletter", tags=["Newsletter"])
